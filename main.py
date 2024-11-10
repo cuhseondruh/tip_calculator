e39a183ab3e62d87ebb57002f92b1f4790431eea
cuhseondruh <casandralopez2017@gmail.com>
2024-11-09 20:17:04 -0600

    prototype

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    page.title = "Tip Calculator"
+    page.theme = ft.Theme(color_scheme_seed="green")
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
@@ -17,12 +16,64 @@ def main(page: ft.Page):
     page.add(
         ft.Row(
             [
+                ft.Text("Bill Amount"),                                
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),        
+        ft.Row(
+            [
+                ft.TextField(label="Enter Bill Amount ex: $100.00", icon=ft.icons.ATTACH_MONEY,border=ft.InputBorder.UNDERLINE),
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),         
+        ft.Row(
+            [
+                ft.Text("Tip % "),
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
+                ft.TextField(value="10", text_align=ft.TextAlign.RIGHT, width=100),
                 ft.IconButton(ft.icons.ADD, on_click=plus_click),
             ],
+            
             alignment=ft.MainAxisAlignment.CENTER,
-        )
+        ),
+        ft.Row(
+            [
+                ft.Text("Tip Amount"),                                
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [
+                ft.TextField(value= "$10", icon=ft.icons.ATTACH_MONEY,border=ft.InputBorder.UNDERLINE),                                
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),        
+        ft.Row(
+            [
+                ft.Text("Total Bill Amount"),                                
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),                    
+        ft.Row(
+            [
+                ft.TextField(value= "$110", icon=ft.icons.ATTACH_MONEY,border=ft.InputBorder.UNDERLINE),                                
+
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [
+                ft.ElevatedButton(text="Generate New"),                                               
+            ],
+            
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),                                 
     )
 
 ft.app(main)
